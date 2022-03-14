a5673a1bd25ea3f7430f02efc3bb5f84895b811e
Oliver <oli@silverchip.com>
2022-03-14 10:57:19 +0000

    add initial locale params

--- src/api/getInitalCategories.js
+++ src/api/getInitalCategories.js
@@ -3,9 +3,11 @@ import getToken from './getToken';
 
 const getInitalCategories = async () => {
     try {
+        const params = { locale: 'sv_SE', country: 'SE' };
         const token = await getToken();
         const headers = { Authorization: token };
-        const { data } = await api.get('browse/categories/', { headers });
+        const { data } = await api.get(`browse/categories/`, { headers, params });
+
         return data.categories;
     } catch (error) {
         console.log({ error });
